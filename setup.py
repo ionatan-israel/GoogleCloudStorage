be487f837a9481a7a6dd367bbf8b7ae0cf633f25
Jonatan Rodriguez <jrperdomoz@Jonatans-Mac-mini.local>
2016-02-04 19:40:28 -0800

    fixed version read

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-exec(open("django_google_storage.py").read())
+exec(open("django_google_storage/version.py").read())
 
 setup(
     name='khipu',
